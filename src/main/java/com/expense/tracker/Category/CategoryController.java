836a407522b17098803fd744c58f3a278e473789
Akshit <akshitpanwar381@gmail.com>
2025-12-25 23:05:25 +0530

    created CategoryController

---
+++ src/main/java/com/expense/tracker/Category/CategoryController.java
@@ -0,0 +1,22 @@
+package com.expense.tracker.Category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/expense-tracker/category")
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addCategory(@RequestBody Category category) {
+        categoryService.addCategory(category);
+        return ResponseEntity.ok("Category added");
+    }
+
+}
--- src/main/java/com/expense/tracker/Expense/ExpenseController.java
+++ src/main/java/com/expense/tracker/Expense/ExpenseController.java
@@ -3,31 +3,19 @@ package com.expense.tracker.Expense;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.expense.tracker.Category.Category;
-
-import com.expense.tracker.Category.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("/api/expense-tracker")
+@RequestMapping("/api/expense-tracker/expense")
 public class ExpenseController {
 
     @Autowired
     ExpenseService expenseService;
 
-    @Autowired
-    CategoryService categoryService;
-
-    @PostMapping("/addCategory")
-    public ResponseEntity<?> addCategory(@RequestBody Category category) {
-        categoryService.addCategory(category);
-        return ResponseEntity.ok("Category added");
-    }
-
-    @PostMapping("/addExpense")
+    @PostMapping("/add")
     public ResponseEntity<?> addExpense(@RequestBody ExpenseDTO expenseRequest) {
         expenseService.addExpense(expenseRequest);
         return ResponseEntity.ok("Expense Added");
