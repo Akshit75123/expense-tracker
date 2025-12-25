2c70919a94f7dbd2bcc0ea146ce948f23a52912a
Akshit <akshitpanwar381@gmail.com>
2025-12-25 22:08:09 +0530

    build addCategory API

--- src/main/java/com/expense/tracker/Expense/ExpenseController.java
+++ src/main/java/com/expense/tracker/Expense/ExpenseController.java
@@ -3,6 +3,8 @@ package com.expense.tracker.Expense;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.expense.tracker.Category.Category;
+import com.expense.tracker.Category.CategoryRepository;
 import com.expense.tracker.Enums.PaymentMethod;
 
 import java.time.LocalDate;
@@ -20,12 +22,28 @@ public class ExpenseController {
     @Autowired
     ExpenseRepository expenseRepository;
 
-    @PostMapping("/add")
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @PostMapping("/addCategory")
+    public ResponseEntity<?> addCategory(@RequestBody Category category) {
+        Category cat = new Category();
+        cat.setName(category.getName());
+        cat.setActive(true);
+
+        categoryRepository.save(cat);
+        return ResponseEntity.ok("Category added successfull");
+    }
+
+    @PostMapping("/addExpense")
     public ResponseEntity<?> addExpense(@RequestBody ExpenseRequestDTO expenseRequest) {
+        Long categoryId = expenseRequest.getCategoryId();
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("Catogory not found"));
 
         Expense expense = new Expense();
         expense.setAmount(expenseRequest.getAmount());
-        expense.setCategory(expenseRequest.getCategory()); // ensure entity
+        expense.setCategory(category);
         expense.setDescription(expenseRequest.getDescription());
         expense.setCreatedAt(LocalDateTime.now());
         expense.setExpenseDate(LocalDate.now());
